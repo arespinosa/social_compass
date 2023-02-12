f3f3eb164e3161efd68b1d4270c80fbeb872e732
aajc <ajarrold@ucsd.edu>
2023-02-12 14:17:30 -0800

    US-1 progress

--- app/src/main/java/com/example/demo5/MainActivity.java
+++ app/src/main/java/com/example/demo5/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.demo5;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LiveData;
 
 public class MainActivity extends AppCompatActivity {
     private TimeService timeService;
@@ -18,13 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /**orientationService = OrientationService.singleton(this);
-        TextView textView = findViewById(R.id.timeTextView);
+        orientationService = OrientationService.singleton(this);
+
+        ConstraintLayout compass = findViewById(R.id.compass);
+
+        LiveData<Float>rad = orientationService.getOrientation();
+
+        float deg = (float) Math.toDegrees(rad.getValue());
 
         orientationService.getOrientation().observe(this, orientation ->{
-            textView.setText(Float.toString(orientation));
-        });*/
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            compass.setRotation(360 - deg);
+        });
+
+
+
+        /*if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
         }
@@ -35,12 +46,12 @@ public class MainActivity extends AppCompatActivity {
         locationService.getLocation().observe(this,loc->{
             textView.setText(Double.toString(loc.first)+" , "+
                     Double.toString(loc.second));
-        });
+        });*/
     }
 
-    /**@Override
+    @Override
     protected void onPause(){
         super.onPause();
         orientationService.unregisterSensorListeners();
-    }*/
+    }
 }
