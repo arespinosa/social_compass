d6f67b008e8610351654733af607ffd220bfdb3f
Philemon Putra <pgputra@ucsd.edu>
2023-02-12 21:58:52 -0800

    still in works of testing

--- app/src/test/java/com/example/demo5/ExampleUnitTest.java
+++ app/src/test/java/com/example/demo5/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.Lifecycle;
@@ -11,37 +13,71 @@ import androidx.test.core.app.ActivityScenario;
 
 import static org.junit.Assert.assertEquals;
 
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     public static final int DEGREES_IN_A_CIRCLE = 360;
 
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
 
-    @Test
     public void testParentHouseAngle() {
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
         //need to have Activity in STARTED state for LiveData observer to be active
         scenario.moveToState(Lifecycle.State.STARTED);
-        scenario.onActivity(activity->{
+        scenario.onActivity(activity -> {
+            //assertEquals(activity.findViewById(R.id.btn_one).performClick(), true);
+
+
             long expected = 12312312L;
 
-            ImageView parentHouse = (ImageView) findViewById(R.id.parentHouse);
+            ImageView parentHouse = activity.findViewById(R.id.parentHouse);
             ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) parentHouse.getLayoutParams();
 
-            TextView textView = findViewById(R.id.timeTextView);
-            locationService.getLocation().observe(this,loc->{
-                textView.setText(Double.toString(loc.first)+" , "+
+            TextView textView = activity.findViewById(R.id.timeTextView);
+
+            LocationService locationService = LocationService.singleton(activity);
+            locationService.getLocation().observe(activity, loc -> {
+                textView.setText(Double.toString(loc.first) + " , " +
                         Double.toString(loc.second));
 
-            //check whether the values are equal
-            assert layoutParams.circleAngle == expected;
+                //check whether the values are equal
+                //assert layoutParams.circleAngle == expected;
+
+                layoutParams.circleAngle = 20;
+
+                assertEquals(layoutParams.circleAngle, 20);
+
+
+            });
         });
     }
 }
+
+//from lab 2 espresso test
+/**
+ * scenario.onActivity(activity -> {
+ *             assertEquals(activity.findViewById(R.id.btn_one).performClick(), true);
+ *         });
+ *         scenario.onActivity(activity -> {
+ *             assertEquals(activity.findViewById(R.id.btn_plus).performClick(), true);
+ *         });
+ *         scenario.onActivity(activity -> {
+ *             assertEquals(activity.findViewById(R.id.btn_one).performClick(), true);
+ *         });
+ *         scenario.onActivity(activity -> {
+ *             assertEquals(activity.findViewById(R.id.btn_equals).performClick(), true);
+ *         });
+ *         scenario.onActivity(activity -> {
+ *             TextView display = (TextView) activity.findViewById(R.id.display);
+ *             assertEquals(display.getText(), "2");
+ *         });
+ */
