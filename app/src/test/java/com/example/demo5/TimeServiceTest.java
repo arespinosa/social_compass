348fccef2f43babe5859bab9177de4d9301fc158
arespinosa <arespinosa@ucsd.edu>
2023-02-13 12:12:20 -0800

    Trying out the test cases

--- app/src/test/java/com/example/demo5/TimeServiceTest.java
+++ app/src/test/java/com/example/demo5/TimeServiceTest.java
@@ -12,27 +12,11 @@ import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class TimeServiceTest {
-    /*@Test
+    @Test
     public void test_time_service(){
-        MutableLiveData<Long> mockDataSource = new MutableLiveData<Long>();
-        TimeService timeService = TimeService.singleton();
-        //Update the singleton instance to use the mockDataSource
-        timeService.setMockTimeSource(mockDataSource);
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
-        //need to have Activity in STARTED state for LiveData observer to be active
-        scenario.moveToState(Lifecycle.State.STARTED);
-        scenario.onActivity(activity->{
-            long expected = 12312312L;
-            //set a random value for the mock data source
-            mockDataSource.setValue(expected);
-            //get value from textview
-            TextView textView = activity.findViewById(R.id.timeTextView);
-            long observed = Long.parseLong(textView.getText().toString());
 
+            assert 1 == 1;
 
-            //check whether the values are equal
-            assert observed == expected;
-        });
     }
-    */
+
 }
