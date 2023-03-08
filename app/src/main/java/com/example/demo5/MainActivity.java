a198487ea46c6287d196af279f894c47ad969e87
Suhaib Mumtaz <suhaib@Suhaibs-MacBook-Pro.local>
2023-03-08 14:23:48 -0800

    conflict resolution

--- app/src/main/java/com/example/demo5/MainActivity.java
+++ app/src/main/java/com/example/demo5/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.demo5;
 
-import android.content.SharedPreferences;
-import android.location.Location;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -35,116 +32,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        locationService = LocationService.singleton(this);
 
-        this.reobserveLocation();
-
-
-        //Setting the time, just testing it out
-        var timeService = TimeService.singleton();
-        var timeData = timeService.getTimeData();
-        timeData.observe(this, this::onTimeChanged);
-    }
-
-    /**
-     * Declaring the longitude and lattitude
-     * TODO: Implement their distances based on the friend's location
-     */
-        double latitude;
-        double longitude;
-
-
-    /**
-     * Reobserving the location of the user by calling onLocationChanged
-     */
-    public void reobserveLocation() {
-            var locationData = locationService.getLocation();
-            locationData.observe(this, this::onLocationChanged);
-        }
-
-
-    /**
-     * If the location changes, will notify the Compass in order to place the friend at the correct
-     * distance of the user
-     * @param latLong : The longitude and latitude of the User
-     */
-    private void onLocationChanged(Pair<Double, Double> latLong) {
-            updateCompassWhenLocationChanges(latLong.first, latLong.second);
-            //this.updateCompassWhenLocationChanges(33.812473718140716,-117.91903852984754);
-        }
-
-    /**
-     * Setting the placement of the friend at the appropriate angle within the constraints of the circle
-     * @param ang
-     */
-    public void settingCircleAngle(int ang) {
-            TextView friendtext = findViewById(R.id.best_friend);
-            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) friendtext.getLayoutParams();
-
-            layoutParams.circleRadius = ang;
-            friendtext.setLayoutParams(layoutParams);
-    }
-
-    /**
-     * This will update the display of the user interface based on the new distance of user
-     * @param longitude
-     * @param latitude
-     */
-    public void updateCompassWhenLocationChanges(Double longitude, Double latitude) {
-        //Initializing the variables to find the miles
-        double mile_total = distanceCalculation(longitude, latitude);
-        double circle_ang;
-
-        //Starting the placement of friend onto screen
-        if(mile_total < 1) {
-            //Place it onto disk 1
-            circle_ang = mile_total/1.0;
-            circle_ang *= 50.0;
-            int ang  = (int) Math.round(circle_ang);
-
-            settingCircleAngle(ang);
-            System.out.println("we out here: disk  1");
-        }
-        else if(mile_total >=1 && mile_total < 10) {
-            //Place it onto disk 2
-            circle_ang = mile_total/10.0;
-            //Space in between 50 - 100
-            circle_ang *= 50;
-
-            circle_ang = circle_ang + 65;
-
-            int ang  = (int) Math.round(circle_ang);
-
-            settingCircleAngle(ang);
-            System.out.println("we out here: disk 2");
-        }
-        else if(mile_total >= 10 && mile_total < 500) {
-            //Place it onto disk 3
-            //490
-            circle_ang = mile_total/490.0;
-            circle_ang *= 100;
-            circle_ang = circle_ang + 135;
-            int ang  = (int) Math.round(circle_ang);
-
-            settingCircleAngle(ang);
-            System.out.println("The angle is " + ang);
-            System.out.println("we out here: disk 3");
-        }
-        else {
-            //Placing it onto disk 4
-
-            circle_ang = mile_total/12427.0;
-            circle_ang *= 200;
-
-            circle_ang = circle_ang + 275;
-
-            int ang  = (int) Math.round(circle_ang);
-
-            settingCircleAngle(ang);
-            System.out.println("we out here: disk 4");
-        }
-
-        setContentView(R.layout.activity_compass);
+        //setContentView(R.layout.activity_compass);
 
         locationService = LocationService.singleton(this);
         bestFriend = new BestFriend();
@@ -157,18 +46,114 @@ public class MainActivity extends AppCompatActivity {
         });
 
         this.reobserveLocation();
+
+        //Setting the time, just testing it out
+        /*var timeService = TimeService.singleton();
+        var timeData = timeService.getTimeData();
+        timeData.observe(this, this::onTimeChanged);*/
     }
 
-    private void reobserveLocation() {
+    /**
+     * Declaring the longitude and lattitude
+     * TODO: Implement their distances based on the friend's location
+     */
+    double latitude;
+    double longitude;
+
+
+    /**
+     * Reobserving the location of the user by calling onLocationChanged
+     */
+    public void reobserveLocation() {
         var locationData = locationService.getLocation();
         locationData.observe(this, this::onLocationChanged);
     }
 
+
+    /**
+     * If the location changes, will notify the Compass in order to place the friend at the correct
+     * distance of the user
+     *
+     * @param latLong : The longitude and latitude of the User
+     */
     private void onLocationChanged(Pair<Double, Double> latLong) {
+        updateCompassWhenLocationChanges(latLong.first, latLong.second);
         TextView locationText = findViewById(R.id.locationText);
         locationText.setText(Utilities.formatLocation(latLong.first, latLong.second));
         userLocation = latLong;
         whenFriendLocationChanges();
+        //this.updateCompassWhenLocationChanges(33.812473718140716,-117.91903852984754);
+    }
+
+    /**
+     * Setting the placement of the friend at the appropriate angle within the constraints of the circle
+     *
+     * @param ang
+     */
+    public void settingCircleAngle(int ang) {
+        TextView friendtext = findViewById(R.id.best_friend);
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) friendtext.getLayoutParams();
+
+        layoutParams.circleRadius = ang;
+        friendtext.setLayoutParams(layoutParams);
+    }
+
+    /**
+     * This will update the display of the user interface based on the new distance of user
+     *
+     * @param longitude
+     * @param latitude
+     */
+    public void updateCompassWhenLocationChanges(Double longitude, Double latitude) {
+        //Initializing the variables to find the miles
+        double mile_total = distanceCalculation(longitude, latitude);
+        double circle_ang;
+
+        //Starting the placement of friend onto screen
+        if (mile_total < 1) {
+            //Place it onto disk 1
+            circle_ang = mile_total / 1.0;
+            circle_ang *= 50.0;
+            int ang = (int) Math.round(circle_ang);
+
+            settingCircleAngle(ang);
+            System.out.println("we out here: disk  1");
+        } else if (mile_total >= 1 && mile_total < 10) {
+            //Place it onto disk 2
+            circle_ang = mile_total / 10.0;
+            //Space in between 50 - 100
+            circle_ang *= 50;
+
+            circle_ang = circle_ang + 65;
+
+            int ang = (int) Math.round(circle_ang);
+
+            settingCircleAngle(ang);
+            System.out.println("we out here: disk 2");
+        } else if (mile_total >= 10 && mile_total < 500) {
+            //Place it onto disk 3
+            //490
+            circle_ang = mile_total / 490.0;
+            circle_ang *= 100;
+            circle_ang = circle_ang + 135;
+            int ang = (int) Math.round(circle_ang);
+
+            settingCircleAngle(ang);
+            System.out.println("The angle is " + ang);
+            System.out.println("we out here: disk 3");
+        } else {
+            //Placing it onto disk 4
+
+            circle_ang = mile_total / 12427.0;
+            circle_ang *= 200;
+
+            circle_ang = circle_ang + 275;
+
+            int ang = (int) Math.round(circle_ang);
+
+            settingCircleAngle(ang);
+            System.out.println("we out here: disk 4");
+        }
     }
 
     public void whenFriendLocationChanges() {
@@ -193,10 +178,11 @@ public class MainActivity extends AppCompatActivity {
         bestFriendRad = Math.atan2(friendLocation.second - userLocation.second, friendLocation.first - userLocation.first);
     }
 
-/**
+    /**
      * Calculating the distance between user and their specific friend
+     *
      * @param longitude : Grabbing the longitude of friend
-     * @param latitude : Grabbing the latitude of friend
+     * @param latitude  : Grabbing the latitude of friend
      * @return The distance calculated
      */
 
@@ -227,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
         lat2 = Math.toRadians(latitude);
         lon2 = Math.toRadians(longitude);
 
-        double distance = Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1))*6371 * 0.621371;
+        double distance = Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1)) * 6371 * 0.621371;
 
         //Will return the distance in miles
         System.out.println("Mile distance = " + distance);
@@ -237,6 +223,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Retrieving the location of the friend
+     *
      * @return : long and lat as a pair of strings
      */
     private Pair<String, String> retrieveFriendLocation() {
