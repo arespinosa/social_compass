0b55a3caede56090c33d69396c5a87dc3a2896ad
Suhaib Mumtaz <suhaib@Suhaibs-MacBook-Pro.local>
2023-03-13 15:03:53 -0700

    ignore livedata

--- app/src/main/java/com/example/demo5/FriendDatabase.java
+++ app/src/main/java/com/example/demo5/FriendDatabase.java
@@ -16,7 +16,7 @@ import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Friend.class}, version = 0)
+@Database(entities = {Friend.class}, version = 1, exportSchema = false)
 @TypeConverters(PairConverter.class)
 public abstract class FriendDatabase extends RoomDatabase {
     static public List<Friend> list = new ArrayList<>();
